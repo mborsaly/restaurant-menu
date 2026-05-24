a5a522d745dc8812874920ff6b77f5b8fb8361e6
Mohamed Borsaly <mohamedborsaly@Mohameds-MacBook-Pro.local>
2026-05-24 01:09:19 -0400

    Menu Screen update

--- src/hooks/useSession.js
+++ src/hooks/useSession.js
@@ -35,7 +35,7 @@ export function useSession() {
           .single()
 
         //if (sessionError) throw new Error('Session not found')
-	if (sessionError) throw new Error(error.toString())
+	if (sessionError) throw new sessionError(error.toString())
 
         // Check if session expired
         const expiresAt = sessionData.expires_at.replace(' ', 'T').replace('+00', 'Z')
