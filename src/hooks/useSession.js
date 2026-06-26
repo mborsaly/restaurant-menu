9d19e7104127015ae4da0c13c11743705551a6d0
Mohamed Borsaly <m_borsaly@yahoo.com>
2026-06-26 06:43:19 +0300

    le-cro-magnon initial commit

--- src/hooks/useSession.js
+++ src/hooks/useSession.js
@@ -22,7 +22,7 @@ export function useSession() {
           const { data: resto } = await supabase
             .from('restaurants')
             .select('*')
-            .eq('slug', 'matsuno-sushi-fusion')
+            .eq('slug', 'le-cro-magnon')
             .single()
           setRestaurant(resto)
 
