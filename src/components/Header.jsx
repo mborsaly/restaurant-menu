c1b44cb2844c069c538f16d10a642dc53f7567a2
Mohamed Borsaly <m_borsaly@yahoo.com>
2026-07-31 02:07:10 +0300

    Fix duplicate translations import in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
 import { useState, useRef, useEffect } from 'react'
-import { t, isRTL } from '../lib/translations'
 import LangSwitcher from './LangSwitcher'
 import { t, isRTL } from '../lib/translations'
 
